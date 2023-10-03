ac63afe19df3fb3e234cec49395166be2f911c2a
fujiwara-michitaka <112914913+fujiwara-michitaka@users.noreply.github.com>
2023-10-03 11:07:57 +0900

    Update marubatu.py

--- marubatu.py
+++ marubatu.py
@@ -7,8 +7,8 @@ random.shuffle(cards)
 
 # ゲーム状態を管理する変数
 selected_cards = []
-found_pairs = 0
-game_over = False
+first_card = None
+flip_timer_id = None
 
 # アプリのタイトル
 st.title("神経衰弱ゲーム")
@@ -17,25 +17,33 @@ st.title("神経衰弱ゲーム")
 for i in range(4):
     for j in range(4):
         card_index = i * 4 + j
-        if not game_over and card_index not in selected_cards:
-            if st.button(f"カード {card_index + 1}"):
-                selected_cards.append(card_index)
+        if card_index not in selected_cards:
+            if first_card is None or len(selected_cards) % 2 == 0:
+                if st.button(f"カード {card_index + 1}"):
+                    selected_cards.append(card_index)
 
 # ゲームロジック
-if len(selected_cards) == 2 and not game_over:
-    if cards[selected_cards[0]] == cards[selected_cards[1]]:
+if len(selected_cards) % 2 == 0:
+    card1 = selected_cards[-2]
+    card2 = selected_cards[-1]
+    if cards[card1] == cards[card2]:
         st.success("正解！")
-        found_pairs += 1
-        selected_cards = []
-
-        # ゲームクリア判定
-        if found_pairs == len(cards) / 2:
-            game_over = True
+        selected_cards.pop()
+        selected_cards.pop()
+        if len(selected_cards) == 16:
             st.success("ゲームクリア！すべてのカードをゲットしました。")
     else:
         st.error("不正解")
+        flip_timer_id = st.empty()
+
+        def reset_error():
+            st.error("")
+            flip_timer_id.empty()
+
+        st.experimental_set_timeout(1, reset_error)
 
 # リセットボタン
-if game_over:
+if len(selected_cards) == 16:
     if st.button("リセット"):
-        selected_cards =
+        selected_cards = []
+        random.shuffle(cards)
