c07a8bb5c9d9be3224cc518b675051b0368b3910
fujiwara-michitaka <112914913+fujiwara-michitaka@users.noreply.github.com>
2023-10-03 10:21:48 +0900

    Update marubatu.py

--- marubatu.py
+++ marubatu.py
@@ -1,58 +1,64 @@
 import streamlit as st
 import random
 
-# 西暦年と対応する出来事のデータ
-years_to_events = {
-    1492: "コロンブスがアメリカを発見",
-    1776: "アメリカ独立宣言",
-    1969: "アポロ11号の月面着陸",
-    2000: "新千年紀の到来",
-    2020: "新型コロナウイルスの世界的な流行",
+# カードの数字と対応する雑学クイズ
+card_data = {
+    1: "雑学クイズ1: 地球上で最も高い山は何か？",
+    2: "雑学クイズ2: 北極圏に生息する動物は何か？",
+    3: "雑学クイズ3: 日本の首都はどこか？",
+    4: "雑学クイズ4: 北海道の最高気温は何度ぐらいになることがあるか？",
+    5: "雑学クイズ5: 酸素の化学記号は何か？",
+    6: "雑学クイズ6: イタリアの首都はどこか？",
+    7: "雑学クイズ7: 水の沸点は何度か？",
+    8: "雑学クイズ8: アメリカの国旗には何本の縞があるか？",
 }
 
-# クイズのカウンターを初期化
-quiz_count = 0
-correct_answers = 0
-max_quiz_count = 5  # クイズの最大数
+# カードを初期化
+cards = list(card_data.keys()) * 2
+random.shuffle(cards)
+
+# ゲーム状態を管理する変数
+selected_cards = []
+found_pairs = 0
+game_over = False
 
 # アプリのタイトル
-st.title("クイズアプリ")
+st.title("神経衰弱ゲーム")
 
-# 5つのクイズを表示
-while quiz_count < max_quiz_count:
-    st.sidebar.markdown(f"問題 {quiz_count + 1}")
-    
-    # ランダムな年と対応する出来事を選択
-    random_year = random.choice(list(years_to_events.keys()))
-    event = years_to_events[random_year]
-    
-    st.write(f"問題 {quiz_count + 1}: {event}")
+# ゲームボードを表示
+for i in range(4):
+    for j in range(4):
+        card_index = i * 4 + j
+        if not game_over and st.button(f"カード {card_index + 1}"):
+            if card_index not in selected_cards:
+                selected_cards.append(card_index)
+        
+        if card_index in selected_cards:
+            st.image("card_front.png", use_container_width=True, key=f"card_front_{card_index}")
+        elif card_index in selected_cards or game_over:
+            card_number = cards[card_index]
+            st.image(f"card_{card_number}.png", use_container_width=True, key=f"card_{card_number}")
 
-    # ユーザーの回答を取得
-    user_answer = st.radio("正しいかどうかを選んでください", options=["〇", "×"])
-
-    # 正誤を判定
-    if random_year in years_to_events and user_answer == "〇":
+# ゲームロジック
+if len(selected_cards) == 2 and not game_over:
+    if cards[selected_cards[0]] == cards[selected_cards[1]]:
         st.success("正解！")
-        correct_answers += 1
-    elif random_year not in years_to_events and user_answer == "×":
-        st.success("正解！")
-        correct_answers += 1
+        found_pairs += 1
+        selected_cards = []
+
+        # ゲームクリア判定
+        if found_pairs == len(card_data):
+            game_over = True
+            st.success("ゲームクリア！すべてのカードをゲットしました。")
+
     else:
-        st.error("不正解。")
+        st.error("不正解")
 
-    # 次の問題へ進むかどうかを確認
-    quiz_count += 1
-    if quiz_count < max_quiz_count:
-        next_question = st.button("次の問題へ進む")
-        if not next_question:
-            break
-
-# 最終的な結果を表示
-st.title("クイズ結果")
-st.write(f"正解した問題数: {correct_answers} / {max_quiz_count}")
-
-if correct_answers == max_quiz_count:
-    st.success("おめでとうございます！すべての問題に正解しました。")
-else:
-    st.error("不正解があります。もう一度挑戦してみてください。")
+# クイズ表示
+if len(selected_cards) == 2 and not game_over:
+    card_number = cards[selected_cards[0]]
+    quiz_question = card_data[card_number]
+    user_answer = st.text_input(quiz_question)
+    
+    if user_answer.lower() == "ok":
+        selected_cards = []
