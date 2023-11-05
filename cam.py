11c803984e83899353a1cc548f5360f2b002b837
fujiwara-michitaka <112914913+fujiwara-michitaka@users.noreply.github.com>
2023-11-05 12:57:18 +0900

    Update cam.py
    ｋ

--- cam.py
+++ cam.py
@@ -1,44 +1,48 @@
 import streamlit as st
 import cv2
+import numpy as np
 import mediapipe as mp
 
-# Mediapipeを使って姿勢と手首の位置を検出するためのライブラリのインポート
-mp_holistic = mp.solutions.holistic
+# MediaPipe Pose instance
+mp_pose = mp.solutions.pose
 
-def main():
-    # Streamlitアプリケーションのタイトルを設定
-    st.title("Hand Distance App")
-    
-    # カメラを初期化
-    cap = cv2.VideoCapture(0)
-    
-    # Mediapipeを使って姿勢検出のためのモデルを初期化
-    holistic = mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
-    
-    while True:
-        # カメラからフレームを取得
-        _, frame_data = cap.read()
-        
-        # Mediapipeを使って姿勢と手首の位置を検出
-        results = holistic.process(frame_data)
-        
-        if results.pose_landmarks:
-            # 左手首と右手首の位置を取得
-            left_wrist = results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_WRIST]
-            right_wrist = results.pose_landmarks.landmark[mp_holistic.PoseLandmark.RIGHT_WRIST]
-            
-            if left_wrist and right_wrist:
-                # 左手首と右手首の距離を計算
-                distance = ((right_wrist.x - left_wrist.x) ** 2 + (right_wrist.y - left_wrist.y) ** 2) ** 0.5
-                
-                # 距離を表示
-                st.write("Distance between wrists:", distance)
-        
-        # OpenCVのカラースペースからRGBに変換
-        frame_data = cv2.cvtColor(frame_data, cv2.COLOR_BGR2RGB)
+# Streamlit page layout
+st.set_page_config(
+    page_title="Pose Detection with Streamlit",
+    layout="wide",
+)
 
-        # NumPy配列をImageに変換
-        st.image(frame_data, use_column_width=True, channels="RGB")
-                
-if __name__ == "__main__":
-    main()
+# Title and description
+st.title("Pose Detection with Streamlit")
+st.write("This is an example of Pose Detection using MediaPipe in Streamlit.")
+
+# Initialize MediaPipe Pose
+pose = mp_pose.Pose()
+
+# Video capture
+cap = cv2.VideoCapture(0)
+
+# Set video width and height
+cap.set(3, 640)
+cap.set(4, 480)
+
+stframe = st.empty()
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        continue
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = pose.process(frame_rgb)
+
+    if results.pose_landmarks:
+        # Draw pose landmarks on the frame
+        mp.drawing_utils.draw_landmarks(
+            frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS
+        )
+
+    # Display the frame with landmarks using st.image
+    stframe.image(frame, channels="BGR")
+
+cap.release()
